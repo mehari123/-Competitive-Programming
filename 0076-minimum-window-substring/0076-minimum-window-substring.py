94eae3aaef1506a2d6e2bbb97026d01d46197bb4
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-02-27 15:18:40 +0300

    Time: 6034 ms (5.00%), Space: 15.7 MB (6.17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,25 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
-        
         str_t=Counter(t)
-        
         left=0
         ans=["" for r in range(len(s))]
-      
         missing=len(t)
         
         for right in range(len(s)+1):
             
             while missing==0:
                 
-                
-                
                 if len(ans)>=(right-left):
                     
                     ans=list(s[left:right])
                 
-                
-                if s[left] in str_t:    
+                if s[left] in str_t: 
+                    
                     str_t[s[left]]+=1
                 
                 if str_t[s[left]]>0:
+                    
                         missing+=1
                     
                 left+=1
@@ -36,8 +32,6 @@ class Solution:
      
                     missing-=1
                 
-                
-                    
         return "".join(ans)
                 
         
