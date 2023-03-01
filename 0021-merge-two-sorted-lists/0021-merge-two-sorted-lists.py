75ad6bd8273c3c39b85e046846ae8f7464ac8c87
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-03-01 16:23:41 +0300

    Time: 324 ms (5.15%), Space: 14.2 MB (23.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,72 +5,91 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-       
-            
-            
-       
+        
+        head1=list1
+        head2=list2
+        new_node=ListNode(0)
+        head3=new_node
+        last=0
         
         if list1==None:
             return list2
         elif list2==None:
             return list1
-        
-        if list1.val<=list2.val:
-            head=list1
-        else:
-            head=list2
-            
-        head2=list2
-        dummy=head
-        new_node=ListNode()
-        last="0"
-        
-        while list1 and list2:
-            if list1.val<=list2.val:
-                
-                new_node.next=list1
-                new_node=new_node.next
-                list1=list1.next
-                last="1"
-            else:
-                new_node.next=list2
-                new_node=new_node.next
-                list2=list2.next
-                last="2"
-                
-        if last=="1":
-            new_node.next=list2
-        else:
-            new_node.next=list1
-        return head
     
             
-#         while list2:
+        def merge(list1,list2):
             
-#             if list1.val>=list2.val:
+            nonlocal new_node
+            nonlocal last
+            if list1==None or list2==None:
                 
-#                 list1.next,list2.next,list2=list2,list1.next,list2.next
-#                 list1=list1.next.next
-#                 print("if")
-#                 print(list1)
-#                 print(head)
+                if last==1:
+                    new_node.next=list2
+                else:
+                    new_node.next=list1
+                    
+                return new_node
+            
+            elif list1.val<=list2.val:
                 
-#             elif list1.val>list2.val:
+                temp=list1.next
+                new_node.next=list1
+                new_node=new_node.next
+                new_node.next=None
                 
-#                 list1.next,list2.next,list2=list2,list1.next,list2.next
-#                 # list1=list1.next
-#                 print("elf")
-#                 print(list1)
-#                 print(list2)
-#             else:
-#             #     # list2.next,list1.next,list1=list1,list2.next,list1.next
-#                 list1.next=list1
-#             #     print("else")
-#             #     print(list1)
+                list1=temp
+                # list1=list1.next
+                last=1
                 
-#         return head
+            else:
+                temp=list2.next
+                new_node.next=list2
+                
+                new_node=new_node.next
+                new_node.next=None
+                
+                list2=temp
+                # list2=list2.next
+                last=2
+            print(list1,list2)
+            return merge(list1,list2)
+                
+        merge(head1,head2)
+        
+        return head3.next
+        
+        
+        
+                
                 
             
+            
+        
+        
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        
+        
+       
+                
+        
+                
+                
+                
+                
+                
                 
                 
                 
