345611c5f49c02b1c62c33cce2e5497386193c90
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-03-30 17:50:48 +0300

    Time: 35 ms (39.13%), Space: 13.9 MB (47.87%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        
+        
+        diff = x ^ y 
+        count = 0
+        while diff > 0 :
+            
+            mod = diff % 2
+            diff = diff // 2
+            
+            if mod == 1 :
+                
+                count +=1
+                  
+        return count
+    
+        
