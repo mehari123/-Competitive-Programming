7182bf8544546e80d7a29c329147784d72181009
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-03-30 17:33:05 +0300

    Time: 35 ms (38.83%), Space: 13.8 MB (40.50%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -2,23 +2,19 @@ class Solution:
     def findComplement(self, num: int) -> int:
         
         ans = []
-        binary = []
         
         while num > 0:
             
             mod = num % 2 
-            
             num = num // 2
             
             if mod == 1 :
                 
                 ans .append(0)
-                binary.append(0)
                 
             else:
                 
                 ans .append(1)
-                binary.append(1)
                 
         ans_num = 0 
         
