f1adb7e66fb074e044212a14a149b802dfd1df3e
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-04-07 11:09:03 +0300

    Create Cities-and-roads.py

---
+++ eolymp/Cities-and-roads.py
@@ -0,0 +1,14 @@
+size = int(input())
+num_roads = []
+
+for row in range(1,size + 1):
+    
+    row_ls = input().strip().split()
+    
+    for index,c in enumerate(row_ls):
+       
+        if c == "1":
+            
+            num_roads.append(index + 1)
+            
+print(len(num_roads)//2)
