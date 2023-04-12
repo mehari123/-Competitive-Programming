747288a0a2c862b0002c9a3dd5644a548c696e89
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-04-12 18:57:12 +0300

    Time: 746 ms (33.93%), Space: 23.2 MB (8.97%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,88 @@
+class Solution:
+    def possibleBipartition(self, k: int, dislikes: List[List[int]]) -> bool:
+        
+        adjecency = defaultdict(set)
+        
+        for dis in dislikes:
+            
+            adjecency[dis[0]].add(dis[1])
+            adjecency[dis[1]].add(dis[0])
+            
+        color = [-1] * k
+        
+        def dfs(node):    
+            
+            for n in adjecency[node]:
+                
+                if color[n-1] == color[node-1]:
+                    
+                    return False
+                
+                elif color[n-1] == -1:
+                    
+                    if color[node-1] == 1:
+                        
+                        color[n-1] = 0
+                        
+                    else:
+                        
+                        color[n-1] = 1
+                    
+                    if not dfs(n):
+                        
+                        return False
+                
+            return True
+                    
+            
+        for i in range(1, len(adjecency)+1):
+            
+            if color[i-1] == -1 :
+                
+                color[i-1] = 1
+                
+                if not dfs(i):
+                    
+                    return False
+                
+        return True
+                
+                
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         for key,value in adjecency.items():
+            
+#             if color[key] == -1:
+#                 color[key] == 1
+                
+#             for node in adj:
+
+#                 if color[node] == -1:
+
+#                     adjecency[dis[0]].add(dis[1])
+
+#                 elif color[node] !=  color[item]:
+
+#                     continue
+
+#                 else:
+
+#                     return False
+                
+#         return True
+            
+            
