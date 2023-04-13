9a676a2ef4027f4553eb0b91432112cd43ede23c
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-04-13 06:18:24 +0300

    Time: 51 ms (66.38%), Space: 16.4 MB (65.92%) - LeetHub

---
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        
+        string = str(root.val)
+        
+        if root.left: 
+            
+            string += "(" + self.tree2str(root.left) + ")"
+            
+        if root.right: 
+            
+            if not root.left: string += "()"
+                
+            string += "(" + self.tree2str(root.right) + ")"
+            
+        return string
