3ffb81740cc7c4e66c4dbe354e7df5d3282f8e44
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-05-02 20:01:44 +0300

    Added solution - LeetHub

---
+++ Heap Sort - GFG/heap-sort.py
@@ -0,0 +1,58 @@
+#User function Template for python3
+from heapq import heappush,heappop
+
+class Solution:
+  
+    #Heapify function to maintain heap property.
+    def heapify(self,arr, n, i):
+        pass
+        
+        # code here
+    
+    #Function to build a Heap from array.
+    def buildHeap(self,arr,n):
+        
+        heap = []
+        for i in arr:
+            
+            heappush(heap,i)
+        # code here
+        for i in range(len(arr)):
+        
+            arr[i] = heappop(heap)
+    #Function to sort an array using Heap Sort.    
+    def HeapSort(self, arr, n):
+        
+        self.buildHeap(arr,n)
+        #code here
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Mohit Kumara
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().HeapSort(arr,n)
+        print(*arr)
+
+# } Driver Code Ends
