57273f09bb1170ebdbf4079041b541eceb17683d
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-05-04 19:31:25 +0300

    Time: 104 ms (12.13%), Space: 23.8 MB (6.58%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,74 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+        self.answer = -float('inf')
+        def dfs(root):
+            if not root:
+                return -float('inf')
+            
+            if not root.left and not root.right:
+                return root.val
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            self.answer = max([self.answer, left, right, root.val, left+right+root.val])
+            return max(left + root.val, right+root.val, root.val)
+            
+        
+        return max(dfs(root), self.answer)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def to_graph(root):
+            
+#             if not root:
+                
+#                 return
+            
+#             if root.left and root.right:
+                
+#                 binary_graph[root.val].add(tuple([root.left.val,root.right.val]))
+#                 to_graph(root.left)
+#                 to_graph(root.right)
+                
+#             elif root.left and not root.right:
+                
+#                 to_graph(root.right)
+#                 binary_graph[root.val].add(root.left.val)
+                
+#             elif root.right and not root.left:
+#                 binary_graph[root.val].add(root.right.val)
+#                 to_graph(root.left)
+                
+#             else:
+                
+#                 binary_graph[root.val].add(1001)
+#                 to_graph(root.left)
+        
+#         to_graph(root)
+#         print(binary_graph)
+                
+                
+        
