560ec223d8b8ac783932ac4a75519c3541efbd1e
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-05-05 10:39:32 +0300

    Time: 628 ms (58.27%), Space: 18 MB (7.66%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+        
+        n = len(grid)
+        visited = set()
+        visited.add((0, 0))
+        queue = deque([(0, 0, 1)])
+        
+        while queue:
+            x, y, steps = queue.popleft()
+            
+            if x == n-1 and y == n-1:
+                return steps
+            
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+                nx, ny = x + dx, y + dy
+                
+                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0 and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    queue.append((nx, ny, steps+1))
+        
+        return -1 
+
+    def shortestPath(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+        
+        n = len(grid)
+        visited = set()
+        visited.add((0, 0))
+        
+        def backtrack(x, y):
+            if x == n-1 and y == n-1:
+                return 1
+            
+            min_steps = float('inf')
+            
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+                nx, ny = x + dx, y + dy
+                
+                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0 and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    steps = backtrack(nx, ny)
+                    visited.remove((nx, ny))
+                    
+                    if steps != -1:
+                        min_steps = min(min_steps, steps + 1)
+            
+            return -1 if min_steps == float('inf') else min_steps
+        
+        return backtrack(0, 0)
