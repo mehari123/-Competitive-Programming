ef4ca7b69ba48fb66460ff36ec6e26139ebd3552
mehari123 <88460961+mehari123@users.noreply.github.com>
2023-05-05 08:57:59 +0300

    Time: 773 ms (30.06%), Space: 17.6 MB (18.84%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,36 +1,45 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
+        
         visit = set(deadends)
         if '0000' in visit:
+            
             return -1
+        
         if target == '0000':
             return 0
 
         visit.add('0000')
-        q = deque(['0000'])
-        lvl = 0
-        while q:
-            lvl += 1
-            for _ in range(len(q)):
-                key = list(map(int, q.popleft()))
+        que = deque(['0000'])
+        level = 0
+        
+        while que:
+            
+            level += 1
+            for _ in range(len(que)):
+                
+                key = list(map(int, que.popleft()))
                 for i in range(4):
-                    key[i] = (key[i] + 1) % 10
-                    tt = ''.join(map(str,key))
-                    if tt not in visit:
-                        q.append(tt)
-                        visit.add(tt)
                     
-                    if target == tt:
-                        return lvl
+                    key[i] = (key[i] + 1) % 10
+                    lock = ''.join(map(str,key))
+                    if lock not in visit:
+                        
+                        que.append(lock)
+                        visit.add(lock)
+                    
+                    if target == lock:
+                        return level
                     
                     key[i] = (key[i]-2) % 10
-                    tt = ''.join(map(str, key))
-                    if tt not in visit:
-                        q.append(tt)
-                        visit.add(tt)
+                    lock = ''.join(map(str, key))
+                    if lock not in visit:
+                        
+                        que.append(lock)
+                        visit.add(lock)
                     
-                    if target == tt:
-                        return lvl
+                    if target == lock:
+                        return level
 
                     key[i] = (key[i]+1) % 10
 
