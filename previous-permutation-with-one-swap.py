d99f429d90c6a97d10c4a123f5dc99948f359fad
Mehari Tesfaye Lakew <88460961+mehari123@users.noreply.github.com>
2023-06-10 11:04:06 +0300

    added previous-permutation-with-one-swap.python

---
+++ previous-permutation-with-one-swap.py
@@ -0,0 +1,32 @@
+class Solution:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:    
+        n = len(arr)
+        min_ = []
+        index = []
+        min_.append(arr[-1])
+        index.append(n-1)
+        
+        for i in range(n-1,-1,-1):
+            
+            if min_[-1] < arr[i]:
+                
+                while  min_ and min_[-1] < arr[i]:
+
+                    min_.pop()
+                    inde = index.pop()
+                    
+                arr[inde],arr[i] = arr[i],arr[inde]
+                return arr
+                    
+            if min_[-1] > arr[i]:
+                
+                min_.append(arr[i])
+                index.append(i)
+                
+            if min_[-1] == arr[i]:
+                
+                min_[-1] = arr[i]
+                index[-1] = i
+                
+              
+        return arr
