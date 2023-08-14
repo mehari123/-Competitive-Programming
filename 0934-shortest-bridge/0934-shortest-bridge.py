3d2552dff1f6187d78fb2579c74c9551b85e95c4
Mehari Tesfaye Lakew <88460961+mehari123@users.noreply.github.com>
2023-08-14 09:08:17 +0300

    Time: 308 ms (99.06%), Space: 19.3 MB (65.26%) - LeetHub

---
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -0,0 +1,50 @@
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        # get the island boundary
+        n = len(grid)
+        queue = deque()
+        directions =  [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def inbound(row, col):
+            nonlocal n
+            return (0 <= row < n and 0 <= col < n)
+
+
+               
+        def dfs(row, col):
+             
+            grid[row][col] = 2
+            queue.append((row,col))
+            
+            for row_change, col_change in directions:
+                new_row = row + row_change
+                new_col = col + col_change
+                
+                if inbound(new_row, new_col) and grid[new_row][new_col] == 1:
+                    dfs(new_row, new_col)
+
+        for r in range(n):
+            if queue:
+                break
+            
+            for c in range(n):
+                if grid[r][c] == 1:
+                    dfs(r,c)
+                    break
+        distance=0
+
+        while queue:
+            for _ in range(len(queue)):
+                row,col=queue.popleft()
+                for row_change,col_change in directions:
+                    new_row=row+row_change
+                    new_col=col+col_change
+                    if inbound(new_row,new_col):
+                        if grid[new_row][new_col]==1:
+                            return distance
+                        if grid[new_row][new_col]==0:
+                            grid[new_row][new_col]=2
+                            queue.append((new_row,new_col))
+            distance+=1
+        return distance
+                
