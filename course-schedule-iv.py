4f29ece3af61bb31bcb3f3d9a5815ecc34e52195
Mehari Tesfaye Lakew <88460961+mehari123@users.noreply.github.com>
2023-10-06 15:59:23 +0300

    added course-schedule-iv.python

---
+++ course-schedule-iv.py
@@ -0,0 +1,48 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        
+        graph = defaultdict(list)
+        
+        for pre in prerequisites:
+            
+            graph[pre[0]].append(pre[1])
+         
+        def is_prime(p):
+        
+            while que:
+
+                parent = que.popleft()
+                childes = graph[parent]
+
+                if  parent not in graph:
+
+                    return False
+
+                for child in childes:
+
+                    if child == p:
+
+                        return True
+                    
+                    elif child not in visited:
+                        
+                        que.append(child)
+                    
+                    visited.add(child)
+                    
+        ans = []           
+        for query in queries:
+            
+            visited = set()
+            que = deque()
+            que.append(query[0])
+            
+            if is_prime(query[1]):
+                
+                ans.append(True)
+                
+            else:
+                
+                ans.append(False)
+                
+        return ans
