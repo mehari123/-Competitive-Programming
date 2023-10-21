a11daf2b70abe41cddeb73ef38c23058abd6a8ff
Mehari Tesfaye Lakew <88460961+mehari123@users.noreply.github.com>
2023-10-21 20:50:03 +0300

    added powx-n.python

---
+++ powx-n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        
+        if n==1 or n==-1 or n==0:
+            
+            return x**n
+        
+        if n%2==0:
+        
+            return self.myPow(x,n//2)**2
+        
+        else:
+            
+            return self.myPow(x,n//2)**2*x
