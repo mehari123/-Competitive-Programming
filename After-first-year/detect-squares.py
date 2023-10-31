702583a105d3ede7f47f9b0511bf07df05d92cd8
Mehari Tesfaye Lakew <ets044012feng@aastu.edu.et>
2023-10-31 09:44:15 +0300

    Added solution for Detect Squares

---
+++ After-first-year/detect-squares.py
@@ -0,0 +1,14 @@
+class DetectSquares:
+    def __init__(self):
+        self.ps = Counter()
+
+    def add(self, point: List[int]) -> None:
+        self.ps[tuple(point)] += 1
+
+    def count(self, point: List[int]) -> int:
+        ans = 0
+        x1, y1 = point
+        for x2, y2 in self.ps:
+            if abs(x1-x2)==abs(y1-y2) and (x1!=x2 or y1!=y2):
+                ans += self.ps[(x2, y2)]*self.ps[(x1, y2)]*self.ps[(x2, y1)]
+        return ans
