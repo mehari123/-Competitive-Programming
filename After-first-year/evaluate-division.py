b22033c8023961cc5e5e40d3b989a8cd1f82ada9
Mehari Tesfaye Lakew <ets044012feng@aastu.edu.et>
2023-12-19 09:28:39 +0300

    Added solution for Evaluate Division

--- After-first-year/evaluate-division.py
+++ After-first-year/evaluate-division.py
@@ -1,28 +1,52 @@
 class Solution:
-    
-    def answer(self, current, end, scalar):
-        if current==end: return scalar
-        self.visited.add(current)
-        if current in self.graph:
-            for i in self.graph[current]:
-                if i[0] not in self.visited:
-                    a=self.answer(i[0],end,scalar*i[1])
-                    if a!=-1: return a
-        return -1
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        self.graph,self.visited={},set()
-        for i in range(len(equations)):
-            if equations[i][0] not in self.graph:
-                self.graph[equations[i][0]]=[]
-            if equations[i][1] not in self.graph:
-                self.graph[equations[i][1]]=[]
-            self.graph[equations[i][0]].append((equations[i][1],1/values[i]))
-            self.graph[equations[i][1]].append((equations[i][0],values[i]))
-        v=[]
-        for i in queries:
-            self.visited=set()
-            if i[0] not in self.graph or i[1] not in self.graph:
-                v.append(-1)
-                continue
-            v.append(1/self.answer(i[0],i[1],1) if i[0]!=i[1] else 1)
-        return v
+        # using shortest path algo
+        graph = defaultdict(set)
+
+        for eq,va in zip(equations,values):
+
+            graph[eq[0]].add((eq[1],va))
+            graph[eq[1]].add((eq[0],1/va))
+
+        ans = []
+        print(graph)
+        for qr in queries:
+
+            que = deque()
+            que.append((qr[0],1))
+            br = False
+            visited = set()
+            while que:
+
+                parent,val = que.popleft()
+                # print(parent,val)
+                visited.add(parent)
+                children = graph[parent]
+                # print(que,children)
+
+                for ch in children:
+
+                    # print(ch[0],qr[1],"ch and qr")
+                    if ch[0] == qr[1]:
+                        ans.append(val * ch[1])
+                        br = True
+                        break
+                    if ch[0] not in visited:
+
+                        que.append((ch[0],val * ch[1]))
+
+                if br:
+                    break
+            if not br:
+
+                ans.append(-1.0)
+        return ans
+            
+
+
+                
+
+                    
+
+
+
