cbb9daf59d0c03027bead92e25cac172d41ca374
Mehari Tesfaye Lakew <ets044012feng@aastu.edu.et>
2024-11-08 07:10:10 +0300

    Added solution for Maximum XOR for Each Query

---
+++ after_grad/maximum-xor-for-each-query.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+ 
+        max_value = (1 << maximumBit) - 1  
+        # print(max_value)
+        cumulative_xor = 0
+        for num in nums:
+            cumulative_xor ^= num
+        
+        result = []
+   
+        for num in reversed(nums):
+            result.append(cumulative_xor ^ max_value)
+            cumulative_xor ^= num
+        
+        return result
